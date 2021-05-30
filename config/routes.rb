8f7e1e49a3bb789f7caded4822f2165aeb009fce
Tammy <tammycwong@gmail.com>
2021-05-30 14:49:30 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create]
-      post '/login' , to: 'auth#create'
-      get '/profile', to: 'users#profile'
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users, only: [:create]
+  #     post '/login' , to: 'auth#create'
+  #     get '/profile', to: 'users#profile'
+  #   end
+  # end
 
   resources :items, only:[:index, :show]
   resources :inventories
